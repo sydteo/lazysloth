4090bbbb7a7946fbac6fe66a1f43b69cd866993f
sai-sharmaine <132867217+sai-sharmaine@users.noreply.github.com>
2024-01-20 12:14:10 +0800

    Add homepage

---
+++ src/screens/home/HomePage.jsx
@@ -0,0 +1,52 @@
+import { Box, Button } from "@mui/material";
+import React, { useState } from "react";
+
+import colours from "../../UI/colours";
+import { Header1, Header2 } from "../../UI/text";
+
+import eat from "../../assets/eat.PNG";
+import sleep from "../../assets/sleep.PNG";
+
+const HomePage = () => {
+  const [isToggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!isToggle);
+  };
+
+  return (
+    <Box
+      sx={{
+        bgcolor: colours.green,
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flex: 1,
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Header1 text="Welcome to Lazy Sloth" />
+      <img
+        src={isToggle ? sleep : eat}
+        alt="Logo"
+        style={{ height: "60%", padding: "20px" }}
+      />
+      <Button
+        variant="contained"
+        sx={{
+          bgcolor: colours.pink,
+          borderRadius: "30px",
+          color: colours.black,
+          border: "2px solid #000",
+          paddingX: "20px",
+        }}
+        onClick={handleToggle}
+      >
+        <Header2 text={isToggle ? "Feed" : "Sleep"} />
+      </Button>
+    </Box>
+  );
+};
+
+export default HomePage;
